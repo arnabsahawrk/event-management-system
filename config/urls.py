58e798890f89f252725e2301dfdcff9d2dececc8
arnabw <arnabsahawrk@gmail.com>
2025-11-30 19:56:04 +0600

    add: home page

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,10 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import include, path
 
+from theme.views import home_view
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", home_view, name="home"),
     path("__reload__/", include("django_browser_reload.urls")),  # last
 ] + debug_toolbar_urls()
