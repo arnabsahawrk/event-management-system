5479e4cbe691a952ea77a0105f397c01e8913d91
arnabw <arnabsahawrk@gmail.com>
2025-12-03 17:27:33 +0600

    fix: database and deployment issue

--- config/settings.py
+++ config/settings.py
@@ -94,7 +94,6 @@ if os.environ.get("DATABASE_URL"):
         "default": dj_database_url.config(
             default=os.environ.get("DATABASE_URL"),
             conn_max_age=600,
-            conn_health_checks=True,
         )
     }
 else:
