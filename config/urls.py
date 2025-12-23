54cd366ec4230df76516a15458ee554f7dd9c3e2
arnabsahawrk <arnabsahawrk@gmail.com>
2025-12-23 20:12:42 +0600

    add: home page

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 
-from theme.views import home_view
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home_view, name="home"),
+    path("", include("apps.core.urls")),
     path("events/", include("apps.events.urls")),
 ]
 
