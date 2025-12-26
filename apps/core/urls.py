8ac26b0469c79cdc2b449c2cfb7b33ac6ec1264f
arnabsahawrk <arnabsahawrk@gmail.com>
2025-12-26 18:38:28 +0600

    refactor: remove bootstrap admin view and related URL from core app

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import bootstrap_admin, home_view, no_permission
+from .views import home_view, no_permission
 
 app_name = "core"
 urlpatterns = [
     path("", home_view, name="home"),
     path("no-permission/", no_permission, name="no-permission"),
-    path("bootstrap-admin/", bootstrap_admin),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User, Group
-from django.http import HttpResponse
 
 
 def home_view(request):
@@ -9,17 +7,3 @@ def home_view(request):
 
 def no_permission(request):
     return render(request, "no-permission.html")
-
-
-def bootstrap_admin(request):
-    if User.objects.filter(username="admin").exists():
-        return HttpResponse("Admin already exists")
-
-    user = User.objects.create_superuser(
-        username="admin", email="admin@example.com", password="1234"
-    )
-
-    admin_group, _ = Group.objects.get_or_create(name="Admin")
-    user.groups.add(admin_group)
-
-    return HttpResponse("Admin user created")
