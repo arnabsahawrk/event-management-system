f42ca15b3d040a5985201d10e7d9ec521b95823d
arnabsahawrk <arnabsahawrk@gmail.com>
2025-12-27 11:33:58 +0600

    feat: add seed_demo_data view and corresponding URL for database seeding

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -7,6 +7,7 @@ from apps.events.views import (
     rsvp_delete,
     rsvp_events,
     rsvp_view,
+    seed_demo_data,
     update_form,
     view_all,
 )
@@ -21,4 +22,5 @@ urlpatterns = [
     path("dashboard/rsvp/<int:event_id>/", rsvp_events, name="rsvp"),
     path("rsvp-view/", rsvp_view, name="rsvp-view"),
     path("rsvp-delete/<int:id>/", rsvp_delete, name="rsvp-delete"),
+    path("seed-demo/", seed_demo_data),
 ]
